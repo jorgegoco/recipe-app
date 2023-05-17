cc6802d8f7d115200deb1edc03bbcc74b68a0737
jorgegoco <jorgegoco70@gmail.com>
2023-05-17 22:03:23 +0200

    Fix public recipes main functionality

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -2,4 +2,8 @@ class PublicRecipesController < ApplicationController
   def index
     @public_recipes = Recipe.includes(:user, :recipe_foods).where(public: true).order(created_at: :desc)
   end
+
+  def show
+    @public_recipe = Recipe.find(params[:id])
+  end
 end
