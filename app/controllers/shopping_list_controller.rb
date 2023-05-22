e5fddc69fdeb04e51c3503a671658778a6274405
jorgegoco <jorgegoco70@gmail.com>
2023-05-22 21:24:07 +0200

    Fix shopping list in recipe

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,24 +1,6 @@
 class ShoppingListController < ApplicationController
+  
   def index
-    @food_amount = 0
-    @total_price = 0
-    @recipe_foods = []
-    @need_foods = RecipeFood.where(recipe_id: params[:recipe_id])
-    @need_foods.each do |need_food|
-      new_food_id = need_food.food_id
-      new_val = Food.where(user: current_user).where(id: new_food_id).first.quantity
-      quantity_needed = new_val - need_food.quantity
-      next if quantity_needed >= 0
-
-      need_food.quantity = quantity_needed * -1
-      @recipe_foods << need_food
-    end
-    @recipe_foods.each do |f|
-      @total_price += f.food.price * f.quantity
-    end
-  end
-
-  def show
     @food_amount = 0
     @total_price = 0
     @recipe_foods = []
@@ -35,6 +17,26 @@ class ShoppingListController < ApplicationController
         end
       end
     end
+    @need_foods.each do |need_food|
+      new_food_id = need_food.food_id
+      new_val = Food.where(user: current_user).where(id: new_food_id).first.quantity
+      quantity_needed = new_val - need_food.quantity
+      next if quantity_needed >= 0
+
+      need_food.quantity = quantity_needed * -1
+      @recipe_foods << need_food
+    end
+    @recipe_foods.each do |f|
+      @total_price += f.food.price * f.quantity
+    end
+  end
+
+  def show
+    @recipe_id = params[:recipe_id]
+    @food_amount = 0
+    @total_price = 0
+    @recipe_foods = []
+    @need_foods = RecipeFood.where(recipe_id: @recipe_id)
     @need_foods.each do |need_food|
       new_food_id = need_food.food_id
       new_val = Food.where(user: current_user).where(id: new_food_id).first.quantity
